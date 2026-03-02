65a87eb2cad664acf930921854031a73c10447e7
bitcoisas <hiro.bitcoisas@gmail.com>
2026-03-02 00:59:55 -0300

    adicionando scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Espero que esse conhecimento me ajude a ter um bom futuro! Estude Bitcoin e seja soberano.</h1>
-    }
+  return (
+    <h1>
+      Espero que esse conhecimento me ajude a ter um bom futuro! Estude Bitcoin
+      e seja soberano.
+    </h1>
+  );
+}
 
-    export default Home;
+export default Home;
