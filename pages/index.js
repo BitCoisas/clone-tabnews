f511ca3fa343cb4b54246d837ea31fc55e7ce1d5
bitcoisas <hirouemura@pm.me>
2026-03-05 01:44:04 -0300

    Teste assinatura 2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 function Home() {
   return (
-    <h1>
-      Espero que esse conhecimento me ajude a ter um bom futuro! Estude Bitcoin
-      e seja soberano.
-    </h1>
+    <>
+      <h1>
+        Espero que esse conhecimento me ajude a ser soberano. Estude o código,
+        seja um melhor ser humano.
+      </h1>
+
+      <p>Testando aqui será que vai aparecer?</p>
+    </>
   );
 }
 
